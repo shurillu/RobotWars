43585f9b1860219282ba30913715c2e1d815e62b
Stefano Ledda <shurillu@tiscalinet.it>
2024-04-16 19:19:53 +0200

    Servo example added

--- Examples/SimpleServo/SimpleServo.ino
+++ Examples/SimpleServo/SimpleServo.ino
@@ -2,7 +2,6 @@
 #define DS3 3
 #define CONTROLLER DS4                     // quale joypad voglio usare: DualShock 3 o 4
 
-#include "Cdrv8833.h"
 #include "ESP32Servo.h"
 #if CONTROLLER == DS4
 #include "PS4Controller.h"
@@ -10,25 +9,20 @@
 #include "Ps3Controller.h"
 #endif
 
-// configurazione motore DC
-#define MOTOR_PIN_A                18      // pin al quale è connesso il pin IN1/IN3 del DRV8833
-#define MOTOR_PIN_B                19      // pin al quale è connesso il pin IN2/IN4 del DRV8833
-#define MOTOR_CHANNEL               1      // canale utilizzato per la gestione del PWM -  
-                                           // DEVE ESSERE DIVERSO PER OGNI MOTORE
-#define MOTOR_ROTATION_INVERTED false      // se vero, inverte il senso di rotazione del motore.
-                                           // Utile se si è invertita la cablatura del motore/se il 
-							               // motore è in posizione inversa rispetto all'altro 
-									       // (coppia motore sinistro/destro)
+// configurazione motore servo
+#define SERVO_PIN                   25     // pin al quale è collegato il segnale del servo
+#define SERVO_CHANNEL                4     // canale utilizzato per la gestione del PWM.
+									       // se non stai cosa stai facendo, lascia questo valoe
+										   // DEVE ESSERE DIVERSO PER OGNI MOTORE/SERVO
 
 #define MY_BT_ADDRESS  "00:11:22:33:44:55" // indirizzo bluetooth utilizzato per l'accoppiamento
-                                           // tra l'ESP32 ed il joypad.
+										   // tra l'ESP32 ed il joypad.
 										   // Inserisci il tuo valore definendo sei byte in formato 
 										   // esadecimale, separati da due punti 
 										   // Questo indirizzo deve essere uguale a quello settato 
 										   // sul joypad mediante l'utility sixaxispairtool
 
-// oggetto per la gestione del motore DC
-Cdrv8833 DCMotor(MOTOR_PIN_A, MOTOR_PIN_B, MOTOR_CHANNEL, MOTOR_ROTATION_INVERTED);
+Servo myServo;   // oggetto per la gestione del servo
 
 // callback che verrà chiamata ogni qualvolta che il joypad si connette
 void onConnect(void) {
@@ -43,22 +37,21 @@ void onDisconnect(void) {
 // callback che verrà chiamata ogni qualvolta che ci sarà un nuovo pacchetto dati inviato dal joypad
 void onPacket(void) {
 
-	int8_t ly, value;
+	uint8_t lTrigger, angle;
 
-	// leggo i valori dello stick sinistro del joypad
+	// leggo il valore del grilletto sinistro (utilizzato per muovere il servo)
 #if CONTROLLER == DS4
-	ly = PS4.data.analog.stick.ly;
+	lTrigger = PS4.data.analog.button.l2;
 #elif CONTROLLER == DS3
-	ly = Ps3.data.analog.stick.ly;
+	lTrigger = Ps3.data.analog.button.l2;
 #endif
 
-	// i valori degli stick variano tra -128 e +127;
-	// la velocità di rotazione del motore varia tra 
-	// -100 (100% indietro) e 100 (100% avanti) 
-	value = map(ly, -128, 127, -100, 100);
+	// il valore dei trigger vanno da 0 (non premuto) a 255 (completamente premuto)
+	// mentre il servo si muove tra zero gradi e 180 gradi
+	angle = map(lTrigger, 0, 255, 0, 180);
 
-	// aggiorno la velocità di rotazione del motore
-	DCMotor.move(value);
+	// aggiorno la posizione del servo
+	myServo.write(angle);
 }
 
 void setup() {
@@ -67,9 +60,9 @@ void setup() {
 	delay(1000);
 	Serial.printf("\n\n---| ESP32 BattleBots controller |---\n\n");
 
-	// setto la modalità slow decay per i motori DC. Questo permette di avere una coppia maggiore 
-	// sull'asse del motore rispetto a quanto potremmo ottenere con la modalità fast decay
-	DCMotor.setDecayMode(drv8833DecaySlow);
+	// configuro l'oggetto myServo specificando su quale pin dell'ESP32 è collegato il segnale del servo
+	// e quale canale PWM utilizzare per la generazione del segnale
+	myServo.attach(SERVO_PIN, SERVO_CHANNEL);
 
 	// inizializza la libreria per la gestione della comunicazione tra il joypad DS4 e la M5Stack Stamp Pico
 #if CONTROLLER == DS4
---
+++ Libraries/README.md
@@ -0,0 +1,12 @@
+# Libraries
+
+
+All the libraries used in the sketches. It is **strongly** suggested to use these libraries (download from here) to compile all projects/sketches.
+
+## Table of contents
+
+- **CDrvo8833**: library for DRV8833 H-Bridge motor driver. [Original repository](https://github.com/shurillu/Cdrv8833).
+- **ESP32Servo**: library for Servo/Brushless motors. [Original repository](https://github.com/RoboticsBrno/ServoESP32).
+- **FastLED**: library for Neopixel LEDs. [Original repository](https://github.com/FastLED/FastLED).
+- **PS3_Controller_ESP32**: library for Sony DualShock 3 joypad/controller. [Original repository](https://github.com/jvpernis/esp32-ps3).
+- **PS4_Controller_ESP32**: library for Sony DualShock 3 joypad/controller. [Original repository](https://github.com/aed3/PS4-esp32).
